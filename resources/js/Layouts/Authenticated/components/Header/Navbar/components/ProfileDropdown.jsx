9b27d3ac79b7ceae89defbdc81b98680e4d2d42b
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2022-11-22 22:23:02 +0100

    Update ProfileDropdown.jsx

--- resources/js/Layouts/Authenticated/components/Header/Navbar/components/ProfileDropdown.jsx
+++ resources/js/Layouts/Authenticated/components/Header/Navbar/components/ProfileDropdown.jsx
@@ -5,7 +5,7 @@ import { Inertia } from "@inertiajs/inertia";
 
 const menuProps = [
     {
-        href: route("home"),
+        href: route("orders.index"),
         method: "post",
         label: "Orders",
         icon: <OrdersIcon />,
@@ -28,18 +28,23 @@ export default function ProfileDropdown() {
         <div className="z-10 dropdown dropdown-end text-right">
             <Menu as="div" className="relative inline-block text-left">
                 <div>
-                    <Menu.Button>
-                        <label
-                            tabIndex={0}
-                            className="btn btn-ghost hover:bg-indigo-300 btn-circle avatar"
-                        >
-                            <div
-                                className="w-10 rounded-full "
-                                aria-hidden="true"
+                    <Menu.Button className="btn btn-ghost hover:bg-indigo-200  btn-circle">
+                        <div className="indicator">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-8 h-8"
                             >
-                                <img src="https://placeimg.com/80/80/people" />
-                            </div>
-                        </label>
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                                />
+                            </svg>
+                        </div>
                     </Menu.Button>
                 </div>
                 <Transition
