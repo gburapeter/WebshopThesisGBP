f638ade7167d6e66f1ccc4b7fe5dea8e2f0e0990
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2022-12-08 20:15:58 +0100

    ziggy

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -6,17 +6,8 @@ import React from "react";
 import { render } from "react-dom";
 import { createInertiaApp } from "@inertiajs/inertia-react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-import route from "ziggy-js";
-// Import generated ziggy.js
-import { Ziggy } from '@/ziggy'
 
 
-
-
-
-// ...
-
-route('home', undefined, undefined, Ziggy);
 const appName =
     window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
 const AuthenticatedTemplate = (page) => <AuthenticatedLayout children={page} />;
@@ -41,7 +32,7 @@ createInertiaApp({
     },
     setup({ el, App, props, }) {
 
-        // global.route = (name, params, absolute, config = Ziggy) => route(name, params, absolute, config);
+
         return render(<App {...props} />, el);
     },
 });
