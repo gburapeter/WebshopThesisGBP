72fad27e6fed0a9508500257afdd8027ffeb1f14
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2022-12-08 17:51:25 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\ProductCategory;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
